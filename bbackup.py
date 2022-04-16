e8e09519ec715de9cef7c7192534a7f72fe43557
Pontus Lurcock <pont@talvi.net>
2022-04-16 16:57:58 +0200

    Reformat source code with black

--- bbackup.py
+++ bbackup.py
@@ -42,10 +42,11 @@ def main():
     #      its own directory with a README explaining what it's for.
     #      Easier than e.g. using YAML for a one-value config :).
 
-    parser = argparse.ArgumentParser('Perform borg backups')
-    parser.add_argument('--dry-run', '-d', action='store_true')
-    parser.add_argument('config_dir', type=str,
-                        help='Configuration and log directory')
+    parser = argparse.ArgumentParser("Perform borg backups")
+    parser.add_argument("--dry-run", "-d", action="store_true")
+    parser.add_argument(
+        "config_dir", type=str, help="Configuration and log directory"
+    )
     args = parser.parse_args()
     config_path = pathlib.Path(args.config_dir)
     exit_code = do_backup(config_path, args.dry_run)
@@ -53,81 +54,86 @@ def main():
 
 
 def do_backup(config_dir: pathlib.Path, dry_run: bool):
-    exclude_file = config_dir.parent.joinpath('exclude.txt')
-    with open(config_dir.joinpath('repo-path.txt'), 'r') as fh:
+    exclude_file = config_dir.parent.joinpath("exclude.txt")
+    with open(config_dir.joinpath("repo-path.txt"), "r") as fh:
         borg_repo = fh.readline().strip()
-    
-    extra_params = ['--dry-run'] if dry_run else []
-    log_file = config_dir.joinpath('logs', 'log')
-    borg_passcommand = 'secret-tool lookup borg-config %s' % config_dir.name
+
+    extra_params = ["--dry-run"] if dry_run else []
+    log_file = config_dir.joinpath("logs", "log")
+    borg_passcommand = "secret-tool lookup borg-config %s" % config_dir.name
     borg_env = dict(os.environ, BORG_PASSCOMMAND=borg_passcommand)
 
-    print('Starting backup to ' + borg_repo)
+    print("Starting backup to " + borg_repo)
 
-    with open(log_file, 'w') as log_fh:
+    with open(log_file, "w") as log_fh:
         create_result = subprocess.run(
             args=[
-                'borg',
-                'create',
-                '--verbose',
-                '--filter', 'AME-x',
-                '--list',
-                '--stats',
-                '--show-rc',
-                '--compression', 'auto,zstd',
-                '--exclude-caches',
-                '--exclude-from', exclude_file,
-            ] + extra_params + [
-                borg_repo + '::{hostname}-test-{now}',
-                pathlib.Path.home().as_posix()
+                "borg",
+                "create",
+                "--verbose",
+                "--filter",
+                "AME-x",
+                "--list",
+                "--stats",
+                "--show-rc",
+                "--compression",
+                "auto,zstd",
+                "--exclude-caches",
+                "--exclude-from",
+                exclude_file,
+            ]
+            + extra_params
+            + [
+                borg_repo + "::{hostname}-test-{now}",
+                pathlib.Path.home().as_posix(),
             ],
             stderr=log_fh,
-            env=borg_env
+            env=borg_env,
         )
-        
-        print('Pruning repository ' + borg_repo)
-    
-        # Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6
-        # monthly archives of THIS machine. The '{hostname}-' prefix is very
-        # important to limit prune's operation to this machine's archives and
-        # not apply to other machines' archives also.
-    
+
+        print("Pruning repository " + borg_repo)
+
+        # Prune repository to 7 daily, 4 weekly and 6 monthly archives. NB: the
+        # '{hostname}-' prefix limits pruning to this machine's archives.
+
         prune_result = subprocess.run(
             args=[
-                'borg',
-                'prune',
-                '--list',
-                '--prefix', '{hostname}-'
-                '--show-rc',
-                '--keep-daily', '7',
-                '--keep-weekly', '4',
-                '--keep-monthly', '6',
-            ] + extra_params + [borg_repo],
+                "borg",
+                "prune",
+                "--list",
+                "--prefix",
+                "{hostname}-" "--show-rc",  # Important! (See above.)
+                "--keep-daily",
+                "7",
+                "--keep-weekly",
+                "4",
+                "--keep-monthly",
+                "6",
+            ]
+            + extra_params
+            + [borg_repo],
             stderr=log_fh,
-            env=borg_env
+            env=borg_env,
         )
-    
-    logrotate_extra_args = ['--debug'] if dry_run else []
-    subprocess.run(args=[
-        'logrotate',
-        '--verbose',
-        '--state', 'logrotate-state'
-        ] + logrotate_extra_args + [
-        'logrotate.conf'
-        ],
-        cwd=config_dir
+
+    logrotate_extra_args = ["--debug"] if dry_run else []
+    subprocess.run(
+        args=["logrotate", "--verbose", "--state", "logrotate-state"]
+        + logrotate_extra_args
+        + ["logrotate.conf"],
+        cwd=config_dir,
     )
-    
+
     for r in create_result, prune_result:
-        print(dict(create='Backup', prune='Prune')[r.args[1]], end='')
-        print(' finished ', end='')
+        print(dict(create="Backup", prune="Prune")[r.args[1]], end="")
+        print(" finished ", end="")
         if r.returncode == 0:
-            print('successfully')
+            print("successfully")
         elif r.returncode == 1:
-            print('with warnings')
+            print("with warnings")
         else:
-            print('with errors')
-    
+            print("with errors")
+
     # use highest exit code as global exit code
     return max(create_result.returncode, prune_result.returncode)
 
@@ -147,11 +153,11 @@ def get_ssh_auth_socket(script_path: str) -> str:
         # "." is the POSIX-compliant version of bash's "source".
         # "echo -n" is not guaranteed by POSIX so we strip the newline instead.
         ". %s; echo $SSH_AUTH_SOCK" % script_path,
-        shell=True
+        shell=True,
     )
 
     return result.strip().decode(sys.stdout.encoding)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
